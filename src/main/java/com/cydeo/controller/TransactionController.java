85c1ebb5b3495c736ba3e238b8234ac7055c8562
supawadeek <latifetor@gmail.com>
2023-11-17 23:11:29 -0500

    Project: Bank Simulation MVC with Validation completed

--- src/main/java/com/cydeo/controller/TransactionController.java
+++ src/main/java/com/cydeo/controller/TransactionController.java
@@ -6,11 +6,13 @@ import com.cydeo.service.AccountService;
 import com.cydeo.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -43,8 +45,13 @@ public class TransactionController {
     @PostMapping("/transfer")
       // write a post method that takes transaction object from the UI
       // complete the transfer and return the same page
-    public String makeTransfer(@ModelAttribute("transaction") Transaction transaction){
+    public String makeTransfer(@Valid @ModelAttribute("transaction") Transaction transaction, BindingResult bindingResult, Model model){
 
+        if (bindingResult.hasErrors()){
+            model.addAttribute("accounts",accountService.ListAllAccount());
+            model.addAttribute("lastTransactions",transactionService.last10Transactions());
+            return "transaction/make-transfer";
+        }
         // having UUID of accounts but need to provide Account_object to the method
         // need to find the Accounts based on the ID that we have and use as a parameter to complete makeTransfer method.
         Account sender = accountService.retrieveById(transaction.getSender());
