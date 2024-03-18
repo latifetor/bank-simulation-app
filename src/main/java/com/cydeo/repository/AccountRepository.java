3f4d8ea57969271cf8f5bb096dcfbf29cfd2949a
supawadeek <latifetor@gmail.com>
2024-03-18 11:21:00 -0400

    Repositories are updated

--- src/main/java/com/cydeo/repository/AccountRepository.java
+++ src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,36 +1,39 @@
 package com.cydeo.repository;
 
 import com.cydeo.dto.AccountDTO;
+import com.cydeo.entity.Account;
 import com.cydeo.exception.RecordNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class AccountRepository {
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    public static List<AccountDTO> accountDTOList = new ArrayList<>();
-
-    public AccountDTO save(AccountDTO accountDTO){
-        accountDTOList.add(accountDTO);
-        return accountDTO;
-    }
-
-    public List<AccountDTO> findAll() {
-        return accountDTOList;
-    }
-
-    public AccountDTO findById(Long id) {
-        /*TASK:
-            - write the method that find the account inside the List
-            - if not: throw RecordNotFoundException
-         */
-
-        return accountDTOList.stream()
-                .filter(account -> account.getId().equals(id))
-                .findAny().orElseThrow(()-> new RecordNotFoundException("Account does not in the database"));
-    }
+//    public static List<AccountDTO> accountDTOList = new ArrayList<>();
+//
+//    public AccountDTO save(AccountDTO accountDTO){
+//        accountDTOList.add(accountDTO);
+//        return accountDTO;
+//    }
+//
+//    public List<AccountDTO> findAll() {
+//        return accountDTOList;
+//    }
+//
+//    public AccountDTO findById(Long id) {
+//        /*TASK:
+//            - write the method that find the account inside the List
+//            - if not: throw RecordNotFoundException
+//         */
+//
+//        return accountDTOList.stream()
+//                .filter(account -> account.getId().equals(id))
+//                .findAny().orElseThrow(()-> new RecordNotFoundException("Account does not in the database"));
+//    }
 
 
 
